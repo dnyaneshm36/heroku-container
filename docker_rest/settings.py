d680c2ca8fb10630c5e07663e4dc333ef63170f3
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-02-04 02:19:27 +0530

    right

--- docker_rest/settings.py
+++ docker_rest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','dnyaneshs-docker.herokuapp.com/' ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','dnyaneshs-docker.herokuapp.com' ]
 
 
 # Application definition
